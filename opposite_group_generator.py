bce2f7d4805593628090da56bf6d0f103cc07d2e
Navid Mahdian <mahdian.navid@gmail.com>
2020-11-09 16:56:33 +0330

    changing file names appended

--- opposite_group_generator.py
+++ opposite_group_generator.py
@@ -1,4 +1,5 @@
 import random
+import os
 high_bound = int(input("number of groups\n")) 
 not_send_exist = True if input("did all the groups send the assignment(y/n) \n") == "y" else False
 print("now enter id of groups who didin't send the assignment\n")
@@ -21,3 +22,17 @@ for i in range(1,len(my_list),2):
 if len(my_list) %2 != 0:
 	pair_list.append((my_list[len(my_list)-1],my_list[0]))
 print(pair_list)
+
+my_dict = {}
+for (a,b) in pair_list:
+	my_dict[a] = b;
+	my_dict[b] = a;
+
+file_names = os.listdir(".")
+for file_name in file_names :
+	splitted = file_name.split("_");
+	g_num = splitted[-1][1:].split(".")[0]
+	if not g_num.isnumeric():
+		continue
+	op_gp = my_dict[int(g_num)]
+	os.rename(file_name,str(op_gp) + ".zip")
